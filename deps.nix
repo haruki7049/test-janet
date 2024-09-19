1b89f6f09a919bfde7d1cb0ffe21aca2b9962dd8
haruki7049 <tontonkirikiri@gmail.com>
2024-09-19 11:22:59 +0900

    chore(flakes): Update

--- deps.nix
+++ deps.nix
@@ -1,7 +1,12 @@
+{ fetchurl }:
+
 [
   {
     name = "circlet";
-    url = "https://github.com/janet-lang/circlet/archive/2e84f542bffde5e0b08789a804fa80f2ebe5771e.tar.gz";
-    hash = "sha256-amG8h214LkUxaWDP70n1io2CuifHOyZuz/wIxO1zPes=";
+    src = fetchurl {
+      url = "https://github.com/janet-lang/circlet/archive/2e84f542bffde5e0b08789a804fa80f2ebe5771e.tar.gz";
+      hash = "sha256-amG8h214LkUxaWDP70n1io2CuifHOyZuz/wIxO1zPes=";
+    };
+    deps = [ ];
   }
 ]
